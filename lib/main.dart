5a144983b316600f781e2c34911b6a0fcb1de3a6
Tauan Coutinho <tauan_coutinho1@hotmail.com>
2022-03-11 18:01:14 -0300

    it was fun

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int count = 0;
+
+  void decrement() {
+    setState(() {
+      if(count > 0) {
+        count--;
+      }
+    });
+    print(count);
+  }
+
+  void increment() {
+    setState(() {
+      if(count < 20){
+        count++;
+      }
+    });
+    print(count);
+  }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/img.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                isFull ? 'Lotado' : 'Pode entrar!',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: isFull ? Colors.red : Colors.green,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              Padding(padding: const EdgeInsets.all(40),
+                child: Text(
+                  '$count',
+                  style: TextStyle(
+                    fontSize: 100,
+                    color: isFull ? Colors.red :  Colors.green,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: isEmpty ? null : decrement,
+                    style: TextButton.styleFrom(
+                        backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                        fixedSize: const Size(100, 100),
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        )),
+                    child: const Text(
+                      'Saiu',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 32,
+                  ),
+                  TextButton(
+                    onPressed: isFull ? null : increment,
+                    style: TextButton.styleFrom(
+                        backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                        fixedSize: const Size(100, 100),
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        )),
+                    child: const Text(
+                      'Entrou',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
